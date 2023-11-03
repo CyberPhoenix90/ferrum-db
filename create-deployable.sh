03be95f5f3f706b644602677afc2b923e308666a
Joao Santos <Joao260690@gmail.com>
2023-11-03 14:50:57 +0100

    fix in deploy script

--- create-deployable.sh
+++ create-deployable.sh
@@ -1,5 +1,6 @@
 cd ferrum-db-server
 rm -rf ferrum
 dotnet publish --runtime linux-x64 -c Release -o ferrum --self-contained=true
+zip -r ferrum-db-server.zip ferrum
 cd ..
-zip -r ferrum-db-server.zip ferrum-db-server/ferrum
+mv ferrum-db-server/ferrum-db-server.zip .
